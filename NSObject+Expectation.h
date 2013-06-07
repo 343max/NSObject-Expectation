b9a72d1c3ce2f0ea3ca4c0dc9eaae50540b5efa0
Max Winde <max.winde@gmail.com>
2013-06-07 14:28:07 +0200

    added some documentation

--- NSObject+Expectation.h
+++ NSObject+Expectation.h
@@ -9,16 +9,67 @@
 
 @interface NSObject (Expectation)
 
+/**
+ Returns self if it is kind of class aClass or nil otherwise
+ @return self or nil
+ */
 - (id)nilUnlessKindOfClass:(Class)aClass;
+
+/**
+ Returns self if it responds to the given selector or nil otherwise
+ @return self or nil
+ */
 - (id)nilUnlessRespondsToSelector:(SEL)selector;
+
+/**
+ Returns self if it conforms to the given protocol or nil otherwise
+ @return self or nil
+ */
 - (id)nilUnlessConformsToProtocol:(Protocol *)aProtocol;
 
+
+#pragma mark - Default Value
+
+/**
+ Returns object if it is kind of class aClass or self otherwise
+ @return object or self
+ */
 - (id)unless:(NSObject *)object isKindOfClass:(Class)aClass;
+
+/**
+ Returns object if it responds to the given selector or nil otherwise
+ @return object or self
+ */
 - (id)unless:(NSObject *)object respondsToSelector:(SEL)selector;
+
+/**
+ Returns objecy if it conforms to the given protocol or nil otherwise
+ @return object or self
+ */
 - (id)unless:(NSObject *)object conformsToProtocol:(Protocol *)aProtocol;
 
+
+#pragma mark - Deprectated
+
+/**
+ Returns self if it is kind of class aClass or nil otherwise
+ @deprecated use nilUnlessKindOfClass: instead
+ @return self or nil
+ */
 - (id)ifIsKindOfClass:(Class)aClass;
+
+/**
+ Returns self if it responds to the given selector or nil otherwise
+ @deprecated use nilUnlessRespondsToSelector: instead
+ @return self or nil
+ */
 - (id)IfRespondsToSelector:(SEL)selector;
+
+/**
+ Returns self if it conforms to the given protocol or nil otherwise
+ @deprecated use nilUnlessConformsToProtocol: instead
+ @return self or nil
+ */
 - (id)IfConformsToProtocol:(Protocol *)aProtocol;
 
 @end
